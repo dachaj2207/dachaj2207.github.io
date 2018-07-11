85b25eccc0d2c4af52a99dddb897f80fdb2e4f29
dachaj2207 <dachaj2207@gmail.com>
2018-07-11 21:57:31 +0200

    Our first commit

---
+++ src/TestGit.java
@@ -0,0 +1,17 @@
+
+public class TestGit {
+
+	public TestGit() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println("fffff");
+		// TODO Auto-generated method stub
+
+	}
+
+}
